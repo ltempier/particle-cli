3cc711275e973075a541218ac33f6371101b11c7
laurent <laurent.tempier@gmail.com>
2015-10-16 09:07:59 +0200

    poc

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
-/**
- ******************************************************************************
- * @file    js/app.js
- * @author  David Middlecamp (david@spark.io)
- * @company Particle ( https://www.particle.io/ )
- * @source https://github.com/spark/particle-cli
- * @version V1.0.0
- * @date    27-December-2013
- * @brief   Main program body.
- ******************************************************************************
-	Copyright (c) 2014 Spark Labs, Inc.  All rights reserved.
+'use strict';
 
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation, either
-	version 3 of the License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Lesser General Public License for more details.
+var WifiUtilities = require('./lib/WifiUtilities.js');
+var dfu = require('./lib/dfu.js');
 
-	You should have received a copy of the GNU Lesser General Public
-	License along with this program; if not, see <http://www.gnu.org/licenses/>.
-	******************************************************************************
- */
+var SerialCommand = require('./commands/SerialCommand.js')
 
-var Interpreter = require('./lib/interpreter.js');
-var cli = new Interpreter();
-cli.supressWarmupMessages = true;
-cli.startup();
-cli.handle(process.argv, true);
+var serialCommand = new SerialCommand();
+
+
+var device = {
+        port: "/dev/cu.usbmodem1451",
+        type: "Photon"
+    },
+    wifiConfig = {
+        channel: "1",
+        mac: "00:1c:7b:cd:56:25",
+        security: "WPA(PSK/AES,TKIP/TKIP) WPA2(PSK/AES,TKIP/TKIP)",
+        signal_level: "-33",
+        ssid: "NUMERICABLE-C93E",
+        password: 'baurentlaptistebangbang'
+    };
+
+serialCommand.customSerialWifiConfig(device, wifiConfig)
+    .then(function (s) {
+        console.log(s)
+    }, function (e) {
+        console.log(e)
+    })
+
+
+// GET DEVICE LIST
+//serialCommand.findDevices(function (d) {
+//    device = d[0]
+//
+//
+//})
+
+// GET WIFI LIST
+//WifiUtilities.scan()
+//    .then(WifiUtilities.cleanApList)
+//    .then(function (wifiList) {
+//
+//        console.log('yo')
+//    });
+
+
